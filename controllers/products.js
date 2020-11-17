807c0b05ef5c77cae3580a422e4d71ce19af649f
Kassandra Torres <ktorres1393@gmail.com>
2020-11-17 18:51:42 -0500

    adds show ctrl

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,52 @@
-//require model
+// (optional) require the model
 const Product = require('../models/product');
-
-//set up module.exports
+// set up module.exports
 module.exports = {
     index,
+    new: newProduct,
+    create,
+    delete: deleteProduct,
+    edit,
+    update,
+    show
 };
-
-//define our controller actions
-
-function index(req, res){
-    Product.find({}, function(err, products){
-        res.render('product/index.ejs', { products })
+// define our controller actions
+function index(req, res) {
+    Product.find({}, function(err, products) {
+        res.render('products/index', { products })
     });
+}
+
+function newProduct(req, res){
+    res.render('products/new');
+}
+
+function create(req, res){
+    Product.create(req.body, function(err, product){
+        res.redirect('/products')
+    })
+}
+function deleteProduct(req, res){
+    Product.findByIdAndDelete(req.params.id, function(err, deletedProducts){
+        res.redirect('/products');
+    })
+}
+
+function edit(req, res) {
+    Product.findById(req.params.id, function(err, product){
+        res.render('products/edit', { product });
+
+    })
+}
+
+function update(req, res){
+   Product.findByIdAndUpdate(req.params.id, req.body, function(err, updatedProduct){
+       res.redirect('/products');
+   }) 
+}
+
+function show(req, res){
+    Product.findById(req.params.id, function(err, product) {
+        res.render('products/show', { product })
+    })
 }
--- routes/products.js
+++ routes/products.js
@@ -6,5 +6,17 @@ const productsCtrl = require('../controllers/products')
 
 router.get('/', productsCtrl.index);
 
+router.get('/new', productsCtrl.new);
+
+router.post('/', productsCtrl.create);
+
+router.delete('/:id', productsCtrl.delete);
+
+router.get('/:id/edit', productsCtrl.edit);
+
+router.put('/:id', productsCtrl.update);
+
+router.get('/:id/show', productsCtrl.show)
+
 module.exports = router;
 
