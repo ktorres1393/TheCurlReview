1b527cf30894b2e52c1679bb7c0d9fca8b17b93a
Kassandra Torres <ktorres1393@gmail.com>
2020-11-17 22:35:59 -0500

    adds review schema

--- models/product.js
+++ models/product.js
@@ -4,6 +4,13 @@ const mongoose = require('mongoose')
 //create a shortcut for Schema
 const Schema = mongoose.Schema;
 
+const reviewSchema = new Schema({
+    content: String,
+    hairType: {
+        type: String
+    }
+}, {timestamps: true});
+
 //create a productSchema
 const productSchema = new Schema({
     name: {
@@ -12,6 +19,7 @@ const productSchema = new Schema({
     },
     description: String,
     website: String,
+    reviews: [ reviewSchema ]
 
 }, { timestamps: true });
 
